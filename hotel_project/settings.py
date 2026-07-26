5ff3183e123cfb6f52e49ed8487023d04517f473
bahdev223 <bahdev223@gmail.com>
2026-07-26 06:18:17 +0000

    fix(settings): default to SQLite, use DATABASE_URL only if explicitly set

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -165,20 +165,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'hotel_project.wsgi.application'
 
 # Database
-if DEBUG:
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
-    }
-else:
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=os.environ.get("DATABASE_URL", "postgres://localhost:5432/hotel_db"),
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
     }
 
 # Password validation
